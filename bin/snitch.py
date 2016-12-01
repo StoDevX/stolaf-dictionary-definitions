00a8b03905f4badbbc7f4235f36bd610b34f7030
Hawken Rives <hawkrives@gmail.com>
2016-12-01 14:47:11 -0600

    fix snitch.py

--- bin/snitch.py
+++ bin/snitch.py
@@ -2,8 +2,8 @@
 import sys
 import requests
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print('usage: snitch.py <msg>')
     sys.exit(1)
 
-requests.post('https://nosnch.in/abf7027315', data={'m': sys.argv[2]})
+requests.post('https://nosnch.in/abf7027315', data={'m': sys.argv[1]})
