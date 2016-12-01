39184d93dfd432dbda8f4ee4c0042949d5f3bb3c
Hawken Rives <hawkrives@gmail.com>
2016-12-01 15:05:41 -0600

    remove unused check-for-changes file

--- bin/check-for-changes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-
-output = subprocess.run(['git', 'status', '--porcelain'], stdout=subprocess.PIPE).stdout
-
-if len(output) == 0:
-    sys.exit(0)
-else:
-    sys.exit(1)
