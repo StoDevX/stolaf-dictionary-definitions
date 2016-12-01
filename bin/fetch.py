f92493810f3c2d3d7114c81a68c1665d8142933e
Hawken Rives <hawkrives@gmail.com>
2016-12-01 14:43:41 -0600

    fix indents

--- bin/fetch.py
+++ bin/fetch.py
@@ -7,14 +7,14 @@ def fetch():
     url_base = 'http://wp.stolaf.edu/sa/'
 
     for page in url_suffixes:
-    url = url_base + page
-    print('getting', url)
-    resp = requests.get(url)
-    soup = Bs(resp.text, 'lxml')
-    defs = soup.find_all(class_='entry-content')[0]
-    pretty = defs.prettify()
-    with open('./data/%s.html' % page, 'w', encoding='utf-8') as outfile:
-        outfile.write(pretty + '\n')
+        url = url_base + page
+        print('getting', url)
+        resp = requests.get(url)
+        soup = Bs(resp.text, 'lxml')
+        defs = soup.find_all(class_='entry-content')[0]
+        pretty = defs.prettify()
+        with open('./data/%s.html' % page, 'w', encoding='utf-8') as outfile:
+            outfile.write(pretty + '\n')
 
 if __name__ == '__main__':
     fetch()
