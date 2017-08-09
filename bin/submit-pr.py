91729b8e6ce8e6200928b2fae74cfccbfa35b434
Drew Volz <volz.drew@gmail.com>
2017-08-09 19:01:34 -0400

    Get ci to push and pull

--- bin/submit-pr.py
+++ bin/submit-pr.py
@@ -3,8 +3,10 @@ from subprocess import run, PIPE
 from datetime import datetime
 import os
 import requests
+import json
 
 date = datetime.now().isoformat()
+pushUrl = 'https://' + os.getenv('GH_KEY') + '@github.com/stodevx/stolaf-dictionary-definitions.git'
 
 run(['git', 'checkout', 'master'], check=True)
 branch = "new-data_" + date.replace(':', '-')
@@ -16,16 +18,16 @@ run(['git', 'add', '--', '*.html'], check=True)
 run(['git', 'commit', '-m', msg], check=True)
 run(['git', 'commit', '--amend', '--author', author, '--no-edit'], check=True)
 
-run(['git', 'push', 'origin', branch], check=True)
+run(['git', 'push', pushUrl, branch], check=True)
 
 # # # # #
 
 url = "https://api.github.com/repos/StoDevX/stolaf-dictionary-definitions/pulls"
-auth = ('hawkrives', os.getenv('GH_KEY'))
-data = {'title': msg, 'head': branch, 'base': 'master'}
+data = json.dumps({"title": msg, "head": branch, "base": 'master'})
 headers = {
+    'Authorization': "token " + os.getenv('GH_KEY'),
     'Content-Type': "application/json",
     'Accept': "application/vnd.github.v3+json",
 }
 
-requests.post(url, data=data, headers=headers, auth=auth)
+requests.post(url, data=data, headers=headers)
