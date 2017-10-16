f478e10e0da55c04fb972b81f8fd42aa4dfe0627
Drew Volz <volz.drew@gmail.com>
2017-10-15 21:20:38 -0400

    Adjust fetch to work after new site rollout
    * Update base url to be https
    * Update class to t-content

--- bin/fetch.py
+++ bin/fetch.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup as Bs
 
 def fetch():
     url_suffixes = ['a-g', 'h-o', 'p-z']
-    url_base = 'http://wp.stolaf.edu/sa/'
+    url_base = 'https://wp.stolaf.edu/sa/'
 
     for page in url_suffixes:
         url = url_base + page
         print('getting', url)
         resp = requests.get(url)
         soup = Bs(resp.text, 'lxml')
-        defs = soup.find_all(class_='entry-content')[0]
+        defs = soup.find_all(class_='t-content')[0]
         pretty = defs.prettify()
         with open('./data/%s.html' % page, 'w', encoding='utf-8') as outfile:
             outfile.write(pretty + '\n')
