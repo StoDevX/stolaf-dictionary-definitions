8fb9eaebe02c82034ed19b9e373c87c0a2b222ee
Drew Volz <volz.drew@gmail.com>
2018-02-28 17:14:53 -0700

    Refactor to make separate files for definitions
    * Issues with parsing (see previous commit)
    * Issues with assumptions (see previous commit)
    * Issues with filenames (see previous commit)

--- bin/fetch.py
+++ bin/fetch.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import requests
+import unicodedata
+import re
 from bs4 import BeautifulSoup as Bs
+
+def prettifyFilename(value):
+    value = str(value)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value).strip().lower()
+    return re.sub(r'[-\s]+', '-', value)
 
 def fetch():
     url_suffixes = ['a-g', 'h-o', 'p-z']
@@ -12,9 +20,19 @@ def fetch():
         resp = requests.get(url)
         soup = Bs(resp.text, 'lxml')
         defs = soup.find_all(class_='t-content')[0]
-        pretty = defs.prettify()
-        with open('./data/%s.html' % page, 'w', encoding='utf-8') as outfile:
-            outfile.write(pretty + '\n')
+
+        for term in defs:
+            try:
+                pretty = term.prettify()
+                name = term.find('strong')
+                if ((name is not None) is True):
+                    # assuming we never have a word that is one character (handles the alphabet)
+                    if(len(name) > 1):
+                        name = prettifyFilename(name.get_text())
+                        with open('./data/%s.html' % name, 'w', encoding='utf-8') as outfile:
+                            outfile.write(pretty + '\n')
+            except AttributeError:
+                pass
 
 if __name__ == '__main__':
     fetch()
